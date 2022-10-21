1f0cdc91a96a184f6bc7bbd2dee7d6bcc0ee7e32
szv99 <szvmx@icloud.com>
2022-10-21 15:02:38 +0200

    kurwa

--- dupa.js
+++
@@ -1,7 +0,0 @@
-function get_video_id(input) {
-return input.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=|\/sandalsResorts#\w\/\w\/.*\/))([^\/&]{10,12})/)[1]; 
-}
-
-gowno = get_video_id('https://youtu.be/3w8pR1ihXOI')
-
-console.log(gowno)
--- socket.js
+++
@@ -1,7 +0,0 @@
-const guwno = require('socket.io');
-const wss = new guwno.Server(7071, { cors: { origin: '*' } })
-
-wss.on('connection', function connection(ws) {
-
-    ws.on('')
-})
